14aedd2f4beda87b143351855ba1caf1811e9ba5
Obi Nwokogba <obi@Madencie.local>
2021-05-23 15:06:50 -0400

    Work on Navigation bar

--- script.js
+++ script.js
@@ -26,33 +26,33 @@ function handleGetData(event) {
     searchText = $("#searchBar").val();
 
     $.ajax({
-            url: `https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa`,
-        })
-        .then(
-            function (returnedData) {
+        url: `https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa`
+    })
+            .then(
+                    function (returnedData) {
 
-                let returnedBackdropImage = returnedData["backdrop_path"];
+                        let returnedBackdropImage = returnedData["backdrop_path"];
 
-                //console.log(returnedData);
-                $renderTitleBox.text(returnedData["original_title"]);
+                        //console.log(returnedData);
+                        $renderTitleBox.text(returnedData["original_title"]);
 
-                let releaseDate = returnedData["release_date"];
-                $renderYearBox.text(releaseDate.substring(0, 4));
+                        let releaseDate = returnedData["release_date"];
+                        $renderYearBox.text(releaseDate.substring(0, 4));
 
-                $renderIMDBRatingBox.text(returnedData["vote_average"]);
+                        $renderIMDBRatingBox.text(returnedData["vote_average"]);
 
-                $renderGenreBox.text(returnedData["genres"][0]["name"]);
+                        $renderGenreBox.text(returnedData["genres"][0]["name"]);
 
-                // Display an Image
-                $moveImageBox1.html(`<img class="mainMovieImage" src="https://image.tmdb.org/t/p/w500${returnedBackdropImage}" alt="movie poster" />`);
+                        // Display an Image
+                        $moveImageBox1.html(`<img class="mainMovieImage" src="https://image.tmdb.org/t/p/w500${returnedBackdropImage}" alt="movie poster" />`);
 
 
-                $renderOverviewBox.text(returnedData["overview"]);
-            },
-            function (error) {
-                console.log("bad request: ", error);
-            }
-        );
+                        $renderOverviewBox.text(returnedData["overview"]);
+                    },
+                    function (error) {
+                        console.log("bad request: ", error);
+                    }
+            );
 }
 
 
@@ -61,35 +61,78 @@ function getTrending(event) {
     let currentTrendingTitle;
 
     $.ajax({
-            url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`,
-        })
-        .then(
-            function (returnedData) {
+        url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`,
+    })
+            .then(
+                    function (returnedData) {
 
-                for (i = 0; i < 17; i++) {
-                    currentTrendingTitle = returnedData["results"][i]["title"];
+                        for (i = 0; i < 17; i++) {
+                            currentTrendingTitle = returnedData["results"][i]["title"];
 
-                    // This check is made because I noticed that occasionally the JSON returns undefined titles.
+                            // This check is made because I noticed that occasionally the JSON returns undefined titles.
 
 
-                    if (currentTrendingTitle === "undefined") {
-                        //alert('Variable "comment" is undefined.');
-                    } else if (currentTrendingTitle === null) {
-                        //alert('Variable "comment" is null.');
-                    } else {
-                        trendingString = trendingString + " &bull; " + currentTrendingTitle;
-                        console.log(returnedData["results"][i]["title"]);
+                            if (currentTrendingTitle === "undefined") {
+                                //alert('Variable "comment" is undefined.');
+                            } else if (currentTrendingTitle === null) {
+                                //alert('Variable "comment" is null.');
+                            } else {
+                                trendingString = trendingString + " &bull; " + currentTrendingTitle;
+                                console.log(returnedData["results"][i]["title"]);
+                            }
+                        }
+                        $trendingBox.html(trendingString);
+                    },
+                    function (error) {
+                        console.log("bad request: ", error);
                     }
-                }
-                $trendingBox.html(trendingString);
-            },
-            function (error) {
-                console.log("bad request: ", error);
-            }
-        );
+            );
 }
 
 $("form").on('submit', handleGetData);
 
+
+
+
+
 // Lets start by showing whats popular
 getTrending();
+
+
+
+function renderTrendingPage() {
+    alert("trending button clicked!");
+    
+    
+    let currentTrendingTitle;
+
+    $.ajax({
+        url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`,
+    })
+            .then(
+                    function (returnedData) {
+
+                        for (i = 0; i < 17; i++) {
+                            currentTrendingTitle = returnedData["results"][i]["title"];
+
+                            // This check is made because I noticed that occasionally the JSON returns undefined titles.
+
+
+                            if (currentTrendingTitle === "undefined") {
+                                //alert('Variable "comment" is undefined.');
+                            } else if (currentTrendingTitle === null) {
+                                //alert('Variable "comment" is null.');
+                            } else {
+                                trendingString = trendingString + " &bull; " + currentTrendingTitle;
+                                console.log(returnedData["results"][i]["title"]);
+                            }
+                        }
+                        $trendingBox.html(trendingString);
+                    },
+                    function (error) {
+                        console.log("bad request: ", error);
+                    }
+            );
+}
+
+$("#trendingButton").on('click', renderTrendingPage);
