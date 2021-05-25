de55ebaaa26b82f415bf40e4be2715d6881dffb0
Obi Nwokogba <obi@Madencie.local>
2021-05-25 16:40:47 -0400

    Improvements to trending data

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 // THEMOVIEDB.org
 //https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa
 
-let searchText, trendingString1Start, trendingString1End, trendingString2;
+let searchText, trendingString1Start, trendingString1End, trendingString2, trendingString;
 
-const apiBaseURL = "https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa";
+const BASE_URL = "https://api.themoviedb.org/3/";
 const apiBasicURL = "https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa";
 
 const $renderTitleBox = $("#renderTitleBox");
@@ -20,127 +20,170 @@ const $pageContent = $("#pageContent");
 const trendingURL = `https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa/trending/{media_type}/{time_window}`;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function handleGetData(event) {
 
-    event.preventDefault();
-    searchText = $("#searchBar").val();
+   event.preventDefault();
+   searchText = $("#searchBar").val();
 
-    $.ajax({
-            url: `https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa`
-        })
-        .then(
-            function (returnedData) {
-                let returnedBackdropImage = returnedData["backdrop_path"];
+   $.ajax({
+      url: `https://api.themoviedb.org/3/movie/550?api_key=0153dd9142cbca8ace6559209c3cf1aa`
+   })
+           .then(
+                   function (returnedData) {
+                      let returnedBackdropImage = returnedData["backdrop_path"];
 
-                //console.log(returnedData);
-                $renderTitleBox.text(returnedData["original_title"]);
+                      //console.log(returnedData);
+                      $renderTitleBox.text(returnedData["original_title"]);
 
-                let releaseDate = returnedData["release_date"];
-                $renderYearBox.text(releaseDate.substring(0, 4));
-                $renderIMDBRatingBox.text(returnedData["vote_average"]);
-                $renderGenreBox.text(returnedData["genres"][0]["name"]);
+                      let releaseDate = returnedData["release_date"];
+                      $renderYearBox.text(releaseDate.substring(0, 4));
+                      $renderIMDBRatingBox.text(returnedData["vote_average"]);
+                      $renderGenreBox.text(returnedData["genres"][0]["name"]);
 
-                // Display an Image
-                $moveImageBox1.html(`<img class="mainMovieImage" src="https://image.tmdb.org/t/p/w500${returnedBackdropImage}" alt="movie poster" />`);
-                $renderOverviewBox.text(returnedData["overview"]);
-            },
-            function (error) {
-                console.log("bad request: ", error);
-            }
-        );
+                      // Display an Image
+                      $moveImageBox1.html(`<img class="mainMovieImage" src="https://image.tmdb.org/t/p/w500${returnedBackdropImage}" alt="movie poster" />`);
+                      $renderOverviewBox.text(returnedData["overview"]);
+                   },
+                   function (error) {
+                      console.log("bad request: ", error);
+                   }
+           );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function getTrending(event) {
 
-    let currentTrendingTitle;
-    let trendingStringMiddle = "";
+   let currentTrendingTitle;
+   let trendingStringMiddle = "";
 
-    trendingString1Start = `<div class="marqueecontainer">
+   trendingString1Start = `<div class="marqueecontainer">
     <div class="marquee-sibling"> Breaking News </div>
     <div class="marquee">
       <ul class="marquee-content-items">`;
 
-    trendingString1End = ` </ul></div></div>`;
+   trendingString1End = ` </ul></div></div>`;
 
-    $.ajax({
-            url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`,
-        })
-        .then(
-            function (returnedData) {
+   $.ajax({
+      url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`
+   })
+           .then(
+                   function (returnedData) {
 
-                for (i = 0; i < 17; i++) {
-                    currentTrendingTitle = returnedData["results"][i]["title"];
+                      for (i = 0; i < 17; i++) {
+                         
+                         currentTrendingTitle = returnedData["results"][i]["title"];
 
-                    // This check is made because I noticed that occasionally the JSON returns undefined titles.
-                    if (currentTrendingTitle === "undefined") {
-                        //alert('Variable "comment" is undefined.');
-                    } else if (currentTrendingTitle === undefined) {
-                        //alert('Variable "comment" is null.');
-                    } else if (currentTrendingTitle === null) {
-                        //alert('Variable "comment" is null.');
-                    } else {
-                        trendingStringMiddle = trendingStringMiddle + " <li>" + currentTrendingTitle + "</li>";
-                        console.log(returnedData["results"][i]["title"]);
-                    }
-                }
-                $trendingBox.html(trendingString);
-            },
-            function (error) {
-                console.log("bad request: ", error);
-            }
-        );
+                         // This check is made because I noticed that occasionally the JSON returns undefined titles.
+                         if (currentTrendingTitle === "undefined") {
+                            //alert('Variable "comment" is undefined.');
+                         } else if (currentTrendingTitle === undefined) {
+                            //alert('Variable "comment" is null.');
+                         } else if (currentTrendingTitle === null) {
+                            //alert('Variable "comment" is null.');
+                         } else {
+                            trendingStringMiddle = trendingStringMiddle + " <li>" + currentTrendingTitle + "</li>";
+                            console.log(returnedData["results"][i]["title"]);
+                         }
+                      }
+                      /*
+                      $trendingBox.html(`${trendingStringStart} 
+                      ${trendingStringMiddle} ${trendingStringEnd}`);
+                    */
+                      $trendingBox.html(`${trendingStringMiddle}`);
+                   },
+                   function (error) {
+                      console.log("bad request: ", error);
+                   }
+           );
 }
 
 $("form").on('submit', handleGetData);
-
-// Lets start by showing whats popular
 getTrending();
+
+
+
+
+
+
+
+
+
+
 
 
 function renderTrendingPage() {
 
-    trendingString2 = "";
-    let currentTrendingTitle, currentVoteAverage;
+   trendingString2 = "";
+   let currentTrendingTitle, currentVoteAverage;
 
-    $.ajax({
-            url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`,
-        })
-        .then(
-            function (returnedData) {
+   
 
-                // Clear the page
-                $pageContent.html(" ");
+   $.ajax({
+      url: `https://api.themoviedb.org/3/trending/all/week?api_key=0153dd9142cbca8ace6559209c3cf1aa`
+   })
+           .then(
+                   function (returnedData) {
+                      // Clear the page
+                      //$pageContent.html(" ");
+                    
+                      for (i = 0; i < 17; i++) {
+                         currentTrendingTitle = returnedData["results"][i]["title"];
+                         currentVoteAverage = returnedData["results"]["0"]["vote_average"];
 
-                for (i = 0; i < 17; i++) {
-                    currentTrendingTitle = returnedData["results"][i]["title"];
-                    currentVoteAverage = returnedData["results"]["0"]["vote_average"];
+                         if (typeof trendingString2 === 'undefined') {
+                            //alert("one undefined found!");
+                         } else {
+                            trendingString2 = trendingString2 + `  <button class="trendingMovieButton">` + currentTrendingTitle + ` &middot; ${currentVoteAverage}</button>`;
+                         }
+                      }
 
-
-
-                    //alert(typeof trendingString);
-
-
-                    if (typeof trendingString === 'undefined') {
-                        //alert("one undefined found!");
-                    } else {
-
-                        trendingString2 = trendingString2 + " &bull; " + currentTrendingTitle;
-
-
-                    }
-                }
-
-                $pageContent.html(`<p class="renderedText">${trendingString2}</p>`);
-            },
-            function (error) {
-                console.log("bad request: ", error);
-            }
-        );
+                      //alert(trendingString2);
+                      $pageContent.html(`<p class="renderedText">${trendingString2}</p>`);
+                   },
+                   function (error) {
+                      console.log("bad request: ", error);
+                   }
+           );
 }
 
 function renderHomePage() {
-    $pageContent.html(``);
-    $pageContent.html(``);
+   $pageContent.html(``);
+   $pageContent.html(``);
 }
 
 $("#trendingButton").on('click', renderTrendingPage);
@@ -148,6 +191,7 @@ $("#homeButton").on('click', renderHomePage);
 
 
 
+renderTrendingPage();
 
 
 
@@ -156,46 +200,50 @@ $("#homeButton").on('click', renderHomePage);
 
 
 
+
+
+
+/*
 
 
 $(function () {
 
-    createMarquee({
+   createMarquee({
 
-        // auto starts on page load
-        autostart: true,
+      // auto starts on page load
+      autostart: true,
 
-        // callback
-        onComplete: function () {},
+      // callback
+      onComplete: function () {},
 
-        // controls the speed at which the marquee moves
-        duration: 30000,
+      // controls the speed at which the marquee moves
+      duration: 30000,
 
-        // right margin between consecutive marquees
-        padding: 20,
+      // right margin between consecutive marquees
+      padding: 20,
 
-        // class of the actual div or span that will be used to create the marquee - 
-        // multiple marquee items may be created using this item's content. 
-        // This item will be removed from the dom
-        marquee_class: '.example-marquee',
+      // class of the actual div or span that will be used to create the marquee - 
+      // multiple marquee items may be created using this item's content. 
+      // This item will be removed from the dom
+      marquee_class: '.example-marquee',
 
-        // the container div in which the marquee content will animate. 
-        container_class: '.example-container',
+      // the container div in which the marquee content will animate. 
+      container_class: '.example-container',
 
-        // a sibling item to the marqueed item  that affects - 
-        // the end point position and available space inside the container. 
-        sibling_class: '.example-sibling',
+      // a sibling item to the marqueed item  that affects - 
+      // the end point position and available space inside the container. 
+      sibling_class: '.example-sibling',
 
-        // Boolean to indicate whether pause on hover should is required. 
-        hover: false,
+      // Boolean to indicate whether pause on hover should is required. 
+      hover: false,
 
-        // 0-1
-        velocity: 0,
+      // 0-1
+      velocity: 0,
 
-        // or right
-        direction: 'left'
+      // or right
+      direction: 'left'
 
-    });
+   });
 
 });
 
@@ -211,11 +259,8 @@ $(function () {
 
 
 
+$('#trendingBox').SimpleMarquee();
 
 
 
-$(function () {
-
-    createMarquee();
-
-});
+*/
